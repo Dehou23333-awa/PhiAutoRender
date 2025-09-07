c2be5e54d5becfe34aa94a66a6bdd5a6bd8b2a99
Dehou23333 <104069850+Dehou23333-awa@users.noreply.github.com>
2025-09-07 21:26:09 +0800

    Refactor: Cleanup

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import subprocess
 import logging
 import time
 
+def cleanup():
+    """清理之前的输出文件和临时文件"""
+    subprocess.run("rd /s /q Render\\output", shell=True)
+    subprocess.run("rd /s /q Render\\temp", shell=True)
+    subprocess.run("rd /s /q Upload\\Covers", shell=True)
+    subprocess.run("rd /s /q Upload\\Covers", shell=True)
+    subprocess.run("rd /s /q Unpack\\output", shell=True)
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='[%(name)s][%(funcName)s] %(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -9,9 +17,7 @@ logger = logging.getLogger(__name__)
 starttime = time.time()
 
 logger.info("Starting Cleanup of previous outputs")
-subprocess.run("rd /s /q Render\\output", shell=True)
-subprocess.run("rd /s /q Render\\temp", shell=True)
-subprocess.run("rd /s /q Upload\\Covers", shell=True)
+cleanup()
 logger.info("Cleanup completed")
 
 logger.info("Starting Unpack process")
